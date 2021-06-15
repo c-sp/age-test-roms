7e2896b89b679b1dddef5eca72b47a44bfe0d35a
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2021-06-15 20:41:39 +0200

    add second speed change frame sequencer alignment test

--- src/sound/switch-speed-ch2-lc-2-cgb.asm
+++ src/sound/switch-speed-ch2-lc-2-cgb.asm
@@ -7,7 +7,8 @@ SECTION "main", ROMX
 WRITE_RESULTS: MACRO
     BEGIN_WRITE_RESULTS
 
-    ; store number of nops before turning on sound & before switching speeds
+    ; store number of nops before switching back to single speed
+    ; & before the second switch to double speed
     ld a, (\1) * $10 + (\2)
     ld [hl+], a
 
@@ -27,17 +28,21 @@ RUN_TEST: MACRO
     ld [rNR52], a ; all sound off
     ld [rDIV], a  ; reset DIV
 
-    NOPS \1
     ld a, $80
     ld [rNR52], a ; sound on
-    ld a, $3B
-    ld [rNR21], a ; channel 2 length counter = 6
+    ld a, $2E
+    ld [rNR21], a ; channel 2 length counter = X
     ld a, $F0
     ld [rNR22], a
     ld a, $00
     ld [rNR23], a
     ld a, $C7
     ld [rNR24], a ; initialize channel 2 with length counter
+
+    SWITCH_SPEED ; switch to double speed
+
+    NOPS \1
+    SWITCH_SPEED ; switch to single speed
 
     NOPS \2
     SWITCH_SPEED ; switch to double speed
@@ -51,17 +56,17 @@ ENDM
 
 
 main:
-    RUN_TEST 0, 0, 4078
-    RUN_TEST 0, 0, 4079
+    RUN_TEST 0, 0, 12269
+    RUN_TEST 0, 0, 12270
 
-    RUN_TEST 0, 1, 4078
-    RUN_TEST 0, 1, 4079
+    RUN_TEST 0, 1, 12269
+    RUN_TEST 0, 1, 12270
 
-    RUN_TEST 1, 0, 4078
-    RUN_TEST 1, 0, 4079
+    RUN_TEST 1, 0, 12269
+    RUN_TEST 1, 0, 12270
 
-    RUN_TEST 1, 1, 4078
-    RUN_TEST 1, 1, 4079
+    RUN_TEST 1, 1, 12269
+    RUN_TEST 1, 1, 12270
 
     FINISH_TEST .EXPECTED_RESULT_CGB_AB
 
@@ -71,14 +76,14 @@ main:
     DB 8 + 8 + 8
     ;
     ;  NOPS1 NOPS2 NOPS3 NR52
-    DB $00,  $EE,  $F2
-    DB $00,  $EF,  $F0
+    DB $00,  $ED,  $F2
+    DB $00,  $EE,  $F0
 
-    DB $01,  $EE,  $F2
-    DB $01,  $EF,  $F0
+    DB $01,  $ED,  $F2
+    DB $01,  $EE,  $F0
 
-    DB $10,  $EE,  $F2
-    DB $10,  $EF,  $F0
+    DB $10,  $ED,  $F2
+    DB $10,  $EE,  $F0
 
-    DB $11,  $EE,  $F2
-    DB $11,  $EF,  $F0
+    DB $11,  $ED,  $F2
+    DB $11,  $EE,  $F0
