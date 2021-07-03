2bb15c8245bef5372beee6d06f661cd5ba60e213
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2021-07-03 19:41:52 +0200

    add oam read timings for double speed (cgbB)

--- src/lcd/oam-read-timing-dmgC-cgbB.asm
+++ src/lcd/oam-read-timing-dmgC-cgbB.asm
@@ -44,14 +44,14 @@ EXPECTED_TEST_RESULTS_CGB:
     DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
     DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
     ; double speed
-    DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
-    DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
-    DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
-    DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
-    DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
-    DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
-    DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
-    DB $FF, $FF, $FF, $FF, $00, $00, $00, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF, $00, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF, $00, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF, $00, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF, $00, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF, $00, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF, $00, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF, $00, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF, $00, $00
 
 
 
@@ -107,45 +107,61 @@ EXPECTED_OAM_VALUES:
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
+    DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
+    DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     ; line 0, 1, 143                  line 0, 1 (next frame)
     DB $00, $FF, $00, $FF, $00, $FF,  $00, $FF, $00, $FF ; SCX 1
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     ; line 0, 1, 143                  line 0, 1 (next frame)
     DB $00, $FF, $00, $FF, $00, $FF,  $00, $FF, $00, $FF ; SCX 2
-    DB $FF, $FF, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
+    DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     ; line 0, 1, 143                  line 0, 1 (next frame)
     DB $00, $FF, $00, $FF, $00, $FF,  $00, $FF, $00, $FF ; SCX 3
-    DB $FF, $FF, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     ; line 0, 1, 143                  line 0, 1 (next frame)
     DB $00, $FF, $00, $FF, $00, $FF,  $00, $FF, $00, $FF ; SCX 4
     DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     ; line 0, 1, 143                  line 0, 1 (next frame)
     DB $00, $FF, $00, $FF, $00, $FF,  $00, $FF, $00, $FF ; SCX 5
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
     DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     ; line 0, 1, 143                  line 0, 1 (next frame)
     DB $00, $FF, $00, $FF, $00, $FF,  $00, $FF, $00, $FF ; SCX 6
     DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
-    DB $FF, $FF, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
     ; line 0, 1, 143                  line 0, 1 (next frame)
     DB $00, $FF, $00, $FF, $00, $FF,  $00, $FF, $00, $FF ; SCX 7
     DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
-    DB $FF, $FF, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
+    DB $FF, $FF, $FF, $FF, $FF, $FF,  $FF, $FF, $FF, $FF
     DB $FF, $00, $FF, $00, $FF, $00,  $FF, $00, $FF, $00
 
 
 
-timed_ly_reads:
+timed_oam_reads:
     ; read at the edge of line 0 mode 3
     DELAY 11
     ld a, [de]  ; 2 m-cycles
@@ -192,49 +208,49 @@ timed_ly_reads:
     ld [hl+], a
     ret
 
-timed_ly_reads_ds:
+timed_oam_reads_ds:
     ; read at the edge of line 0 mode 3
-    DELAY 30
-    ld a, [de]  ; 2 m-cycles
-    ld [hl+], a ; 2 m-cycles
+    DELAY 31
+    ld a, [de]        ; 2 m-cycles
+    ld [hl+], a       ; 2 m-cycles
     ; read at the edge of line 0 mode 0
-    DELAY 172 / 2 - 4
-    ld a, [de]
-    ld [hl+], a
+    DELAY 172 / 2 - 4 ; 82 m-cycles
+    ld a, [de]        ; 2 m-cycles
+    ld [hl+], a       ; 2 m-cycles
 
     ; read at the edge of line 1 mode 2
-    DELAY 96
+    DELAY 97
     ld a, [de]
     ld [hl+], a
     ; read at the edge of line 1 mode 0
-    DELAY (4 + 80 + 172) / 2 - 4
+    DELAY (80 + 172) / 2 - 4
     ld a, [de]
     ld [hl+], a
 
-    ; read at the edge of line 143 mode 2
-    DELAY 96 + (141 * 456) / 2
+    ; read at the edge of line 143 mode 2   !!!!
+    DELAY 98 + (141 * 456) / 2
     ld a, [de]
     ld [hl+], a
     ; read at the edge of line 143 mode 0
-    DELAY (4 + 80 + 172) / 2 - 4
+    DELAY (80 + 172) / 2 - 4
     ld a, [de]
     ld [hl+], a
 
     ; read at the edge of line 154 mode 2 (line 0 next frame)
-    DELAY 96 + (10 * 456) / 2
+    DELAY 98 + (10 * 456) / 2
     ld a, [de]
     ld [hl+], a
     ; read at the edge of line 154 mode 0 (line 0 next frame)
-    DELAY (4 + 80 + 172) / 2 - 4
+    DELAY (80 + 172) / 2 - 4
     ld a, [de]
     ld [hl+], a
 
     ; read at the edge of line 155 mode 2 (line 1 next frame)
-    DELAY 96
+    DELAY 98
     ld a, [de]
     ld [hl+], a
     ; read at the edge of line 155 mode 0 (line 1 next frame)
-    DELAY (4 + 80 + 172) / 2 - 4
+    DELAY (80 + 172) / 2 - 4
     ld a, [de]
     ld [hl+], a
     ret
@@ -262,7 +278,8 @@ compact_test_results:
     ld bc, OAM_VALUES
     ld de, EXPECTED_OAM_VALUES
     ld hl, TEST_RESULTS
-    COMPACT_RESULTS_LINES 16, 4, BYTES_PER_LINE
+    COMPACT_RESULTS_LINES 8, 4, BYTES_PER_LINE
+    COMPACT_RESULTS_LINES 8, 6, BYTES_PER_LINE
     ret
 
 
@@ -275,10 +292,10 @@ run_test:
     ld [de], a
 
     FOR SCX, 8
-        TEST SCX, timed_ly_reads, 0
-        TEST SCX, timed_ly_reads, 1
-        TEST SCX, timed_ly_reads, 2
-        TEST SCX, timed_ly_reads, 3
+        TEST SCX, timed_oam_reads, 0
+        TEST SCX, timed_oam_reads, 1
+        TEST SCX, timed_oam_reads, 2
+        TEST SCX, timed_oam_reads, 3
     ENDR
 
     CP_IS_CGB
@@ -290,14 +307,16 @@ run_test:
     SWITCH_SPEED
 
     FOR SCX, 8
-        TEST SCX, timed_ly_reads_ds, 0
-        TEST SCX, timed_ly_reads_ds, 1
-        TEST SCX, timed_ly_reads_ds, 2
-        TEST SCX, timed_ly_reads_ds, 3
+        TEST SCX, timed_oam_reads_ds, 0
+        TEST SCX, timed_oam_reads_ds, 1
+        TEST SCX, timed_oam_reads_ds, 2
+        TEST SCX, timed_oam_reads_ds, 3
+        TEST SCX, timed_oam_reads_ds, 4
+        TEST SCX, timed_oam_reads_ds, 5
     ENDR
 
     SWITCH_SPEED
     call compact_test_results
-    ;MEMCPY TEST_RESULTS, OAM_VALUES + 0 * 4 * BYTES_PER_LINE, 16 * 8
+    ;MEMCPY TEST_RESULTS, OAM_VALUES + 8 * 4 * BYTES_PER_LINE, 16 * 8
     ld hl, EXPECTED_TEST_RESULTS_CGB
     ret
