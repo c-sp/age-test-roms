c48be65f25175e2d1485c64c9aed67e22f22575f
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2021-10-21 17:04:46 +0200

    add missing files

---
+++ src/vram/vram-read-ncmBE.asm
@@ -0,0 +1,9 @@
+; Verified:
+;   2021-10-21 pass: CPU CGB E - CPU-CGB-06
+;   2021-10-21 pass: CPU CGB B - CPU-CGB-02
+;   2021-10-21 fail: DMG-CPU C (blob) - DMG-CPU-08
+;
+INCLUDE "hardware.inc"
+DEF CART_COMPATIBILITY EQU CART_COMPATIBLE_DMG
+DEF CART_REQUIRES_NON_CGB_MODE EQU 1
+INCLUDE "vram/vram-read.inc"
