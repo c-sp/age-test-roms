28611e79d61a7f659fbf261a854a27c47e508b87
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2021-11-30 20:49:36 +0100

    adjust 'verified' in BG LCDC tests

--- src/m3-bg-lcdc/m3-bg-lcdc-nocgb.asm
+++ src/m3-bg-lcdc/m3-bg-lcdc-nocgb.asm
@@ -1,7 +1,7 @@
 ; Verified:
-;   2021-11-12: CPU CGB E - CPU-CGB-06 (non-CGB mode)
-;   2021-11-12: CPU CGB B - CPU-CGB-02 (non-CGB mode)
-;   2021-11-12: DMG-CPU C (blob) - DMG-CPU-08
+;   2021-11-29: CPU CGB E - CPU-CGB-06 (non-CGB mode)
+;   2021-11-29: CPU CGB B - CPU-CGB-02 (non-CGB mode)
+;   2021-11-29: DMG-CPU C (blob) - DMG-CPU-08
 ;
 INCLUDE "hardware.inc"
 DEF CART_COMPATIBILITY EQU CART_COMPATIBLE_DMG
