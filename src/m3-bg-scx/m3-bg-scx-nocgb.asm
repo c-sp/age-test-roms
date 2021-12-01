f13be08aa0f461d4a9cb2b29c83850532d6c7797
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2021-12-01 20:24:37 +0100

    adust BG SCX test

--- src/m3-bg-scx/m3-bg-scx-nocgb.asm
+++ src/m3-bg-scx/m3-bg-scx-nocgb.asm
@@ -1,7 +1,7 @@
 ; Verified:
-;   2021-11-12: CPU CGB E - CPU-CGB-06 (non-CGB mode)
-;   2021-11-12: CPU CGB B - CPU-CGB-02 (non-CGB mode)
-;   2021-11-12: DMG-CPU C (blob) - DMG-CPU-08
+;   2021-12-01: CPU CGB E - CPU-CGB-06 (non-CGB mode)
+;   2021-12-01: CPU CGB B - CPU-CGB-02 (non-CGB mode)
+;   2021-12-01: DMG-CPU C (blob) - DMG-CPU-08
 ;
 INCLUDE "hardware.inc"
 DEF CART_COMPATIBILITY EQU CART_COMPATIBLE_DMG
