3b27d1c1fb149efe71569b1ffe7bc16757a72636
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2021-12-23 13:12:09 +0100

    add window timing tests

---
+++ src/stat-mode-window/stat-mode-window-cgbBE.asm
@@ -0,0 +1,9 @@
+; Verified:
+;   2021-12-22 pass: CPU CGB E - CPU-CGB-06
+;   2021-12-22 pass: CPU CGB B - CPU-CGB-02
+;   2021-12-22 fail: DMG-CPU C (blob) - DMG-CPU-08
+;
+INCLUDE "hardware.inc"
+DEF CART_COMPATIBILITY EQU CART_COMPATIBLE_DMG_GBC
+DEF CGB EQU 1
+INCLUDE "stat-mode-window/stat-mode-window.inc"
