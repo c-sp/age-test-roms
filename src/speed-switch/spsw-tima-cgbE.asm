f43a9767d5e36fb29c06781120a72984c3dd768b
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2022-02-14 20:57:10 +0100

    minor speed switch test rom adjustments

--- src/speed-switch/spsw-tima-cgbE.asm
+++ src/speed-switch/spsw-tima-cgbE.asm
@@ -1,7 +1,7 @@
 ; Verified:
-;   2022-02-06 pass: CPU CGB E - CPU-CGB-06
-;   2022-02-06 fail: CPU CGB B - CPU-CGB-02
-;   2022-02-06 fail: DMG-CPU C (blob) - DMG-CPU-08
+;   2022-02-14 pass: CPU CGB E - CPU-CGB-06
+;   2022-02-14 fail: CPU CGB B - CPU-CGB-02
+;   2022-02-14 fail: DMG-CPU C (blob) - DMG-CPU-08
 ;
 INCLUDE "hardware.inc"
 DEF CART_COMPATIBILITY EQU CART_COMPATIBLE_GBC
