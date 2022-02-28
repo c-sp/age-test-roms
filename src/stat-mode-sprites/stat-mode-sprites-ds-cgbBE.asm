3d9ed379c325dc07ec6c4e7c8c33e3d31a5ad8f6
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2022-02-28 20:17:51 +0100

    adjust sprite timing test

--- src/stat-mode-sprites/stat-mode-sprites-ds-cgbBE.asm
+++ src/stat-mode-sprites/stat-mode-sprites-ds-cgbBE.asm
@@ -1,7 +1,7 @@
 ; Verified:
-;   2022-02-23 pass: CPU CGB E - CPU-CGB-06
-;   2022-02-23 pass: CPU CGB B - CPU-CGB-02
-;   2022-02-23 fail: DMG-CPU C (blob) - DMG-CPU-08
+;   2022-02-24 pass: CPU CGB E - CPU-CGB-06
+;   2022-02-24 pass: CPU CGB B - CPU-CGB-02
+;   2022-02-24 fail: DMG-CPU C (blob) - DMG-CPU-08
 ;
 INCLUDE "hardware.inc"
 DEF CART_COMPATIBILITY EQU CART_COMPATIBLE_GBC
