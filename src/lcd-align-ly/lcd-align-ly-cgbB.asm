fa49fb3f603a199e14333cd824fa5fa4ee6e7267
Christoph Sprenger <66515552+c-sp@users.noreply.github.com>
2022-03-13 13:40:17 +0100

    add LCD alignment test (LY)

---
+++ src/lcd-align-ly/lcd-align-ly-cgbB.asm
@@ -0,0 +1,8 @@
+; Verified:
+;   2022-03-13 fail: CPU CGB E - CPU-CGB-06
+;   2022-03-13 pass: CPU CGB B - CPU-CGB-02
+;   2022-03-13 fail: DMG-CPU C (blob) - DMG-CPU-08
+;
+INCLUDE "hardware.inc"
+DEF CART_COMPATIBILITY EQU CART_COMPATIBLE_GBC
+INCLUDE "lcd-align-ly/lcd-align-ly.inc"
